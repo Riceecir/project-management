368f55cdad0564c16688449025248f2f2054e531
DESKTOP-35O2P55\Riceecir <fengfan.zh.cn@gamail.com>
2021-05-13 22:52:08 +0800

    工程搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css';
-import { Login } from './screens/login'
-import { ProjectList } from './screens/project-list'
+import "./App.css";
+import { Login } from "./screens/login";
+import { ProjectList } from "./screens/project-list";
 
 function App() {
   return (
