cc865c3f712b48a425c8c0b87b10f9aed642c790
DESKTOP-35O2P55\Riceecir <fengfan.zh.cn@gamail.com>
2021-06-04 10:51:37 +0800

    bus fix

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -14,7 +14,7 @@ export const ProjectScreen = () => {
       <Link to="epic">任务</Link>
 
       <Routes>
-        <Navigate to="board" />
+        <Navigate to="board" replace={true} />
         <Route path="/board" element={<BoardScreen />}></Route>
         <Route path="/epic" element={<EpicScreen />}></Route>
       </Routes>
