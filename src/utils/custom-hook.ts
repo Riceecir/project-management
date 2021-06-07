6464cfe1ceab23a6b5fcc7e46b93395873e3cd64
DESKTOP-35O2P55\Riceecir <fengfan.zh.cn@gamail.com>
2021-06-07 11:03:15 +0800

    增加 useUndo hook

--- src/utils/custom-hook.ts
+++ src/utils/custom-hook.ts
@@ -203,3 +203,75 @@ export const useMountedRef = () => {
 
   return mountedRef;
 };
+
+/* use undo hook */
+export const useUndo = <T>(initialPresent: T) => {
+  /* const [past, setPast] = useState<T[]>([])
+  const [present, setPresent] = useState<T>(initialPresent)
+  const [future, setFuture] = useState<T[]>([]) */
+
+  /* 互相关联的 state 可以合并成一个 state */
+  const [state, setState] = useState({
+    past: [] as T[],
+    present: initialPresent as T,
+    future: [] as T[],
+  });
+
+  const canUndo = state.past.length > 0;
+  const canRedo = state.future.length > 0;
+
+  const undo = useCallback(() => {
+    setState((currentState) => {
+      const { past, present, future } = currentState;
+      if (past.length === 0) return currentState;
+
+      const previous = past[past.length - 1];
+      const newPast = past.slice(0, past.length - 1);
+
+      return {
+        past: newPast,
+        present: previous,
+        future: [present, ...future],
+      };
+    });
+  }, []);
+
+  const redo = useCallback(() => {
+    setState((currentState) => {
+      const { past, present, future } = currentState;
+      if (future.length === 0) return currentState;
+
+      const next = future[0];
+      const newFuture = future.slice(1);
+
+      return {
+        past: [...past, present],
+        present: next,
+        future: newFuture,
+      };
+    });
+  }, []);
+
+  const set = useCallback((newPresent: T) => {
+    setState((currentState) => {
+      const { past, present } = currentState;
+      if (newPresent === present) return currentState;
+
+      return {
+        past: [...past, newPresent],
+        present: newPresent,
+        future: [],
+      };
+    });
+  }, []);
+
+  const reset = useCallback((newPresent: T) => {
+    setState(() => ({
+      past: [],
+      present: newPresent,
+      future: [],
+    }));
+  }, []);
+
+  return [state, { undo, redo, set, reset, canUndo, canRedo }];
+};
